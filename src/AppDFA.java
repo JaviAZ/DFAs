fd2fbc9c65f21b615653e6d682085933df2a0059
Javier Alcazar <javialcazafra@gmail.com>
2018-03-22 13:49:31 +0000

    almost done

--- src/AppDFA.java
+++ src/AppDFA.java
@@ -1,10 +1,13 @@
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Scanner;
 import java.util.InputMismatchException;
-
+/*  Student number: C1628112
+	Student name: Javier Alcazar-Zafra
+	Deterministic finite automaton application
+ */
 public class AppDFA {
 	public static void main (String [] args){
+		Scanner in = new Scanner(System.in);
 		if(args.length==0 || args.length>2) {
 			System.err.println("A file name must be input.");
 			System.err.println("For example: ");
@@ -12,13 +15,37 @@ public class AppDFA {
 		} if(args.length==2) {
 			DFA m = new DFA(args[0]);
 			DFA n = new DFA(args[1]);
-			DFA mn = getIntersect(m,n);
-			System.out.println(mn.getEncoding());
+			System.out.println("1.- Intersection.\n2.- Symmetric difference.\n3.- Equivalence");
+			try {
+				int answer = in.nextInt();
+				switch (answer) {
+					case 1:
+						DFA mn = getIntersect(m,n);
+						System.out.println(mn.getEncoding());
+						break;
+					case 2:
+						DFA SymDif = symmetricDifference(m, n);
+						System.out.println(SymDif.getEncoding());
+						break;
+					case 3:
+						DFA equiv = symmetricDifference(m, n);
+						if(equiv.getEndStates().size()==0) {
+							System.out.println("Equivalent.");
+						} else {
+							System.out.println("Not equivalent.");
+						}
+						break;
+					default:
+						System.out.println("Please input 1, 2, 3.");
+						break;
+				}
+			} catch(InputMismatchException e){
+				System.err.println("Please input 1, 2, 3.");
+			}
 		} else {
 			String filename1=args[0];
 			DFA m = new DFA(filename1);
-			Scanner in = new Scanner(System.in);
-			System.out.println("1.- Print M.\n2.- Complementation.");
+			System.out.println("1.- Print M.\n2.- Complementation.\n3.- Check for language.");
 			try {
 				int answer = in.nextInt();
 				switch (answer) {
@@ -35,8 +62,16 @@ public class AppDFA {
 						System.out.println("\nConjugate: ");
 						printDFA(n);
 						break;
+					case 3:
+						ArrayList <ArrayList <String>> visitedAndLanguage = new ArrayList<>(hasLanguage(m));
+						if(visitedAndLanguage.get(0).size()==0){
+							System.out.println("Language empty.");
+						}else {
+							System.out.println("Language not empty - "+printArray(visitedAndLanguage.get(1))+" accepted.");
+						}
+						break;
 					default:
-						System.out.println("Please input 1 or 2.");
+						System.out.println("Please input 1, 2 or 3.");
 						break;
 				}
 			} catch(InputMismatchException e){
@@ -45,66 +80,84 @@ public class AppDFA {
 		}
 	}
 
-	private static String printArray(String [] strArray){
+	private static String printArray(ArrayList <String> strArray){
 		StringBuilder temp=new StringBuilder();
-		for(int i = 0;i < strArray.length;i++){
-			if(i>0 && i<strArray.length){
+		for(int i = 0;i < strArray.size();i++){
+			if(i>0 && i<strArray.size()){
 				temp.append(", ");
 			}
-			temp.append(strArray[i]);
+			temp.append(strArray.get(i));
 		}
 		return temp.toString();
 	}
-
-	private static String printArray(String [][] strArray){
+	private static String print2DArray(ArrayList <ArrayList<String>> strArray){
 		StringBuilder temp=new StringBuilder();
-		for(int i = 0;i < strArray.length;i++){
-			for(int j = 0;j < strArray[i].length;j++){
-				temp.append(strArray[i][j]);
-				if(j<strArray[i].length-1){
+		for(int i = 0;i < strArray.size();i++){
+			for(int j = 0;j < strArray.get(i).size();j++){
+				temp.append(strArray.get(i).get(j));
+				if(j<strArray.get(i).size()-1){
 					temp.append(" ");
 				}
 			}
-			if(i<strArray.length-1){
+			if(i<strArray.size()-1){
 				temp.append("; ");
 			}
 		}
 		return temp.toString();
 	}
-
 	private static void printDFA(DFA m) {
 		System.out.println("States: "+printArray(m.getStates()));
 		System.out.println("Alphabet: "+printArray(m.getAlphabet()));
-		System.out.println("Transitions: "+printArray(m.getTransitions()));
+		System.out.println("Transitions: "+print2DArray(m.getTransitions()));
 		System.out.println("End states: "+printArray(m.getEndStates()));
 	}
 
-	private static String [] findConjugateEnds(DFA m) {
-		ArrayList<String> temp1 = new ArrayList <>(Arrays.asList(m.getStates()));
-		temp1.removeAll(Arrays.asList(m.getEndStates()));
-		return temp1.toArray(new String [temp1.size()]);
+	//Task 1
+	private static ArrayList <String> findConjugateEnds(DFA m) {
+		ArrayList<String> temp1 = new ArrayList <>(m.getStates());
+		temp1.removeAll(m.getEndStates());
+		return temp1;
 	}
 
+	//Task 2
 	private static DFA getIntersect(DFA m, DFA n) {
 		DFA x = new DFA();
 		ArrayList <String> tempStates = new ArrayList<>();
 		ArrayList <ArrayList<String>> tempTransitions = new ArrayList<>();
 		ArrayList <String> tempEndStates = new ArrayList<>();
-		x.setAlphabet(m.getAlphabet());
-		x.setStartState(m.getStartState()+n.getStartState());
+		ArrayList <String> tempAlphabet = new ArrayList<>();
+		for(int i = 0;i < m.getAlphabet().size();i++) {
+			if(n.getAlphabet().contains(m.getAlphabet().get(i))) {
+				tempAlphabet.add(m.getAlphabet().get(i));
+			}
+		}
+		if(tempAlphabet.size()==0) {
+			System.err.println("DFAs don't have any matches in their alphabets.");
+			System.exit(1);
+		}
+		x.setAlphabet(tempAlphabet);
+		x.setStartState(m.getStartState() + n.getStartState());
 		tempStates.add(x.getStartState());
-		String [] [] t1=m.getTransitions();
-		String [] [] t2=n.getTransitions();
-		String [] [] tempT;
-		int t1Index;
-		int t2Index;
-		for(int i = 0;i<m.getStates().length*n.getStates().length;i++) {
+		ArrayList <ArrayList<String>> t1 = m.getTransitions();
+		ArrayList <ArrayList<String>> t2 = n.getTransitions();
+		int t1Index, t2Index;
+		for(int i = 0;i<m.getStates().size()*n.getStates().size();i++) {
 			if (tempStates.size() > i) {
 				tempTransitions.add(new ArrayList<>());
-				for(int j = 0;j<x.getAlphabet().length;j++) {
-					t1Index = m.getStateIndex(("" + tempStates.get(i).charAt(0)));
-					t2Index = n.getStateIndex(("" + tempStates.get(i).charAt(1)));
-					String newState = t1[t1Index][j] + t2[t2Index][j];
+				for(int j = 0;j<x.getAlphabet().size();j++) {
+					int t1Pos = 0;
+					int t2Pos = 1;
+					t1Index = m.getStates().indexOf(("" + tempStates.get(i).substring(0,t1Pos)));
+					t2Index = n.getStates().indexOf(("" + tempStates.get(i).substring(t2Pos,tempStates.get(i).length())));
+					while(t1Index==-1) {
+						t1Pos++;
+						t1Index = m.getStates().indexOf(tempStates.get(i).substring(0,t1Pos));
+					}
+					while(t2Index==-1) {
+						t2Pos++;
+						t2Index = n.getStates().indexOf(tempStates.get(i).substring(t2Pos,tempStates.get(i).length()));
+					}
+					String newState = t1.get(t1Index).get(j) + t2.get(t2Index).get(j);
 					if (!tempStates.contains(newState)) {
 						tempStates.add(newState);
 					}
@@ -112,22 +165,76 @@ public class AppDFA {
 				}
 			}
 		}
-		for(int i = 0;i < m.getEndStates().length;i++) {
-			for(int j = 0;j < n.getEndStates().length;j++) {
-				String [] mEnd = m.getEndStates();
-				String [] nEnd = n.getEndStates();
-				if(tempStates.contains(mEnd[i]+nEnd[j])) {
-					tempEndStates.add(mEnd[i]+nEnd[j]);
+		for(int i = 0;i < m.getEndStates().size();i++) {
+			for(int j = 0;j < n.getEndStates().size();j++) {
+				ArrayList <String> mEnd = m.getEndStates();
+				ArrayList <String> nEnd = n.getEndStates();
+				if(tempStates.contains(mEnd.get(i)+nEnd.get(j))) {
+					tempEndStates.add(mEnd.get(i)+nEnd.get(j));
 				}
 			}
 		}
-		x.setStates(tempStates.toArray(new String [tempStates.size()]));
-		tempT = new String [tempTransitions.size()][];
-		for(int i = 0;i < tempTransitions.size();i++) {
-				tempT[i] = tempTransitions.get(i).toArray(new String [tempTransitions.get(i).size()]);
-		}
-		x.setTransitions(tempT);
-		x.setEndStates(tempEndStates.toArray(new String [tempEndStates.size()]));
+		x.setStates(tempStates);
+		x.setTransitions(tempTransitions);
+		x.setEndStates(tempEndStates);
 		return x;
 	}
+
+	//Task 3
+	private static DFA symmetricDifference(DFA m, DFA n) {
+		DFA mn = getIntersect(m, n);
+		ArrayList <String> conjugateEnds = new ArrayList <>(findConjugateEnds(mn));
+		ArrayList <String> mEnd = m.getEndStates();
+		ArrayList <String> nEnd = n.getEndStates();
+		for(int i = 0;i < conjugateEnds.size();i++) {
+			if(!mEnd.contains(conjugateEnds.get(i).charAt(0)+"") && !nEnd.contains(conjugateEnds.get(i).charAt(1)+"")){
+				conjugateEnds.remove(i);
+				i--;
+			}
+		}
+		return new DFA(conjugateEnds, mn);
+	}
+
+	//Task 4
+	private static ArrayList <ArrayList <String>> hasLanguage (DFA m) {
+		String currState = m.getStartState();
+		ArrayList <ArrayList <String>> visitedAndLanguage = new ArrayList<>();
+
+		visitedAndLanguage.add(new ArrayList<>());
+		visitedAndLanguage.add(new ArrayList<>());
+		visitedAndLanguage.get(0).add(currState);
+		if(m.getEndStates().contains(currState)) {
+			return visitedAndLanguage;
+		}else {
+			visitedAndLanguage = recursiveHasLanguage(m, currState, visitedAndLanguage);
+			if (!m.getEndStates().contains(visitedAndLanguage.get(0).get(visitedAndLanguage.get(0).size()-1))) {
+				visitedAndLanguage.get(0).removeAll(visitedAndLanguage.get(0));
+				visitedAndLanguage.get(1).removeAll(visitedAndLanguage.get(1));
+			}
+			return visitedAndLanguage;
+		}
+	}
+	private static ArrayList <ArrayList <String>> recursiveHasLanguage (DFA m, String currState, ArrayList <ArrayList<String>> visitedAndLanguage) {
+		ArrayList <String> endStates = m.getEndStates();
+		ArrayList <String> currTrans = m.getTransitions().get(m.getStates().indexOf(currState));
+		for(int i = 0;i < m.getAlphabet().size();i++) {
+			currState = currTrans.get(i);
+			if(!visitedAndLanguage.get(0).contains(currState)) {
+				visitedAndLanguage.get(0).add(currState);
+				visitedAndLanguage.get(1).add(m.getAlphabet().get(i));
+				if (endStates.contains(currState)) {
+					return visitedAndLanguage;
+				} else {
+					recursiveHasLanguage(m, currState, visitedAndLanguage);
+					if (endStates.contains(visitedAndLanguage.get(0).get(0))) {
+						return visitedAndLanguage;
+					}
+				}
+			}
+		}
+		return visitedAndLanguage;
+	}
+
+	//Task 5
+
 }
--- src/DFA.java
+++ src/DFA.java
@@ -1,136 +1,139 @@
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.InputMismatchException;
+import java.util.NoSuchElementException;
 import java.util.Scanner;
 import java.lang.StringBuilder;
 
 public class DFA {
-	private String [] states, alphabet, endStates;
-	private String [][] transitions;
+	private ArrayList <String> states = new ArrayList <>();
+	private ArrayList <String> alphabet = new ArrayList <>();
+	private ArrayList <String> endStates = new ArrayList <>();
+	private ArrayList <ArrayList<String>> transitions = new ArrayList <>();
 	private String startState;
 
-	public DFA () { }
-	public DFA (String [] newEndStates,DFA otherDFA){
+	DFA () { }
+	DFA (ArrayList <String> newEndStates,DFA otherDFA){
 		setStates(otherDFA.getStates());
 		setAlphabet(otherDFA.getAlphabet());
 		setTransitions(otherDFA.getTransitions());
 		setStartState(otherDFA.getStartState());
 		setEndStates(newEndStates);
 	}
-	public DFA (String filename) {
+	DFA (String filename) {
 		File file = new File(filename);
 		try {
 			Scanner sc = new Scanner(file);
 			int numStates = sc.nextInt();
-			states = new String [numStates];
 			for(int i = 0;i < numStates;i++){
-				states[i] = sc.next();
+				states.add(sc.next());
 			}
 			int sizeAlphabet = sc.nextInt();
-			alphabet = new String [sizeAlphabet];
 			for(int i=0;i<sizeAlphabet;i++){
-				alphabet[i] = sc.next();
+				alphabet.add(sc.next());
 			}
-			transitions = new String [numStates][sizeAlphabet];
 			for(int i = 0;i < numStates;i++){
+				transitions.add(new ArrayList<>());
 				for(int j = 0;j < sizeAlphabet;j++){
-					transitions [i][j] = sc.next();
+					transitions.get(i).add(sc.next());
 				}
 			}
 			startState = sc.next();
 			int numEndStates = sc.nextInt();
-			endStates = new String [numEndStates];
 			for(int i = 0;i < numEndStates;i++){
-				endStates [i] = sc.next();
+				endStates.add(sc.next());
 			}
-		}catch (FileNotFoundException e) {
-			System.err.println("File was not found, please try again.");
+		}catch (Exception e) {
+			if(e instanceof FileNotFoundException) {
+				System.err.println("File was not found, please try again.");
+				System.exit(1);
+			}else if(e instanceof InputMismatchException || e instanceof NoSuchElementException) {
+				System.err.println("File does not contain a valid encoding.");
+				System.exit(1);
+			}else {
+				System.err.println(e.getMessage());
+			}
 		}
 	}
 
-	public String [] getEndStates(){
+	ArrayList <String> getEndStates(){
 		return endStates;
 	}
-	public void setEndStates(String [] newEndStates) {
-		endStates=newEndStates;
+	void setEndStates(ArrayList <String> newEndStates) {
+		endStates = newEndStates;
 	}
 
-	public String [] getStates(){
+	ArrayList <String> getStates(){
 		return states;
 	}
-	public void setStates(String [] newStates) {
-		states=newStates;
+	void setStates(ArrayList <String> newStates) {
+		states = newStates;
 	}
 
-	public String [] getAlphabet(){
+	ArrayList <String> getAlphabet(){
 		return alphabet;
 	}
-	public void setAlphabet(String [] newAlphabet) {
-		alphabet=newAlphabet;
+	void setAlphabet(ArrayList <String> newAlphabet) {
+		alphabet = newAlphabet;
 	}
 
-	public String [][] getTransitions(){
+	ArrayList <ArrayList<String>> getTransitions(){
 		return transitions;
 	}
-	public void setTransitions(String [][] newTransitions) {
-		transitions=newTransitions;
+	void setTransitions(ArrayList <ArrayList<String>> newTransitions) {
+		transitions = newTransitions;
 	}
 
-	public String getStartState(){
+	String getStartState(){
 		return startState;
 	}
-	public void setStartState(String newStartStates) {
-		startState=newStartStates;
+	void setStartState(String newStartStates) {
+		startState = newStartStates;
 	}
 
-	public String getEncoding() {
+	String getEncoding() {
 		StringBuilder temp = new StringBuilder();
 		String returnString = "";
-		returnString+=states.length+"\n";
-		for(int i = 0;i < states.length;i++) {
-			temp.append(states[i]);
-			if(i<states.length-1){
+		returnString+=states.size()+"\n";
+		for(int i = 0;i < states.size();i++) {
+			temp.append(states.get(i));
+			if(i<states.size()-1){
 				temp.append(" ");
 			}
 		}
 		returnString+=temp.toString()+"\n";
-		returnString+=alphabet.length+"\n";
+		returnString+=alphabet.size()+"\n";
 		temp.delete(0,temp.length());
-		for(int i = 0;i < alphabet.length;i++) {
-			temp.append(alphabet[i]);
-			if(i<alphabet.length-1){
+		for(int i = 0;i < alphabet.size();i++) {
+			temp.append(alphabet.get(i));
+			if(i<alphabet.size()-1){
 				temp.append(" ");
 			}
 		}
 		temp.append("\n");
-		for(int i = 0;i < transitions.length;i++){
-			for(int j = 0;j < transitions[i].length;j++){
-				temp.append(transitions[i][j]);
-				if(j<transitions[i].length-1){
+		for(int i = 0;i < transitions.size();i++){
+			for(int j = 0;j < transitions.get(i).size();j++){
+				temp.append(transitions.get(i).get(j));
+				if(j<transitions.get(i).size()-1){
 					temp.append(" ");
 				}
 			}
-			if(i<transitions.length-1){
+			if(i<transitions.size()-1){
 				temp.append("\n");
 			}
 		}
 		returnString+=temp.toString()+"\n";
 		returnString+=startState+"\n";
-		returnString+=endStates.length+"\n";
+		returnString+=endStates.size()+"\n";
 		temp.delete(0,temp.length());
-		for(int i = 0;i < endStates.length;i++) {
-			temp.append(endStates[i]);
-			if(i<endStates.length-1){
+		for(int i = 0;i < endStates.size();i++) {
+			temp.append(endStates.get(i));
+			if(i<endStates.size()-1){
 				temp.append(" ");
 			}
 		}
 		returnString+=temp.toString();
 		return returnString;
-	}
-
-	public int getStateIndex(String s) {
-		ArrayList<String> temp1 = new ArrayList <>(Arrays.asList(states));
-		return temp1.indexOf(s);
 	}
 }
